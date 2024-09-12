2f5383cc3b0d27aebf75cb7e40c124570b458811
p20199 <p20199@unipi.gr>
2024-09-12 20:20:52 +0300

    Connected SQL Server DB with configs, first Models and Controller functional

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InformationSystems.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        // DbSet is a collection of stock entities that can be queried
+        public DbSet<Models.Stock> Stocks { get; set; }
+        public DbSet<Models.Comment> Comments { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using InformationSystems.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    // Use SQL Server
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
